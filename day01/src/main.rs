3e9a3cad01a37cf273f0782b6572dd98ab2427bd
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2022-12-23 17:21:36 -0800

    day 1 part 1

---
+++ day01/src/main.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut elves = vec![0];
+    let mut elf_index = 0;
+    let file = File::open("input").unwrap(); // fail if the file fails to open
+    let lines = io::BufReader::new(file).lines();
+    for line in lines {
+        if let Ok(calories) = line {
+            if calories != ""
+            {
+                elves[elf_index] += calories.parse::<i32>().unwrap();
+            } else {
+                elves.push(0);
+                elf_index += 1;
+            }
+        }
+    }
+    println!("{}", elves.iter().max().unwrap());
+}

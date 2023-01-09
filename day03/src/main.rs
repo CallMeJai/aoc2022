7575939a1961389748bb26528fc1e7a2dffa5137
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2023-01-09 01:18:58 -0800

    day 3 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -6,22 +6,40 @@ use std::fs::File;
 fn main() {
     let mut total_priority = 0;
     if let Ok(knapsacks) = read_lines("input") {
+        // there must be a better way to read 3 lines at a time
+        // but i don't want to look for it
+        let mut ctr = 0;
+        let mut line1 = String::new();
+        let mut line2 = String::new();
+        let mut line3 = String::new();
         for line in knapsacks {
-            if let Ok(knapsack) = line {
-                total_priority += priority(find_common_char(knapsack.split_at(knapsack.len()/2)).unwrap());
+            if ctr == 0 {
+                line1 = line.unwrap();
+            } else if ctr == 1 {
+                line2 = line.unwrap();
+            } else {
+                line3 = line.unwrap();
+                total_priority += priority(find_common_char((&line1, &line2, &line3)).unwrap());
+                ctr = -1;
             }
+            ctr += 1;
         }
     }
     println!("{}", total_priority);
 }
 
-fn find_common_char((first, second): (&str, &str)) -> Option<char> {
+fn find_common_char((first, second, third): (&str, &str, &str)) -> Option<char> {
     let mut map = HashMap::new();
     for character in first.chars() {
         map.entry(character).or_insert(1);
     }
     for character in second.chars() {
         if map.contains_key(&character) {
+            map.insert(character, 2);
+        }
+    }
+    for character in third.chars() {
+        if map.get(&character) == Some(&2) {
             return Some(character);
         }
     }
